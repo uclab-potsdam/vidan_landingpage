be3fff752c96b38d9d01a6616059151d256f2692
more-ginger <francesca1morini@gmail.com>
2023-04-25 16:39:35 +0200

    remove lint to avoid faulty production

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,5 @@
 module.exports = {
-  publicPath: './',
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.(pdf)(\?.*)?$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                name: 'calls/[name].[hash:8].[ext]'
-              }
-            }
-          ]
-        }
-      ]
-    }
-  },
-  chainWebpack: config => {
-    config.module.rule('eslint').use('eslint-loader').options({
-      fix: true
-    })
-  }
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/vidan/' :
+    '/'
 }
